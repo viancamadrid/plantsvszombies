37c5fb0cfc0b38313a87a0c522af64f3649a2279
viancamadrid <viancamadrid@unitec.edu>
2016-03-24 16:53:05 -0600

    cosas

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include "plant.h"
 #include "zombie.h"
+#include "guisantes.h"
+#include "patata.h"
+#include "tiraagua.h"
+#include "znormal.h"
+#include "zarmadura.h"
+#include "ztirafuego.h"
+#include <iostream>
 
 int main(int argc, char const *argv[]){
 	
